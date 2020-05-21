148939c68d1513202dabdef3f78dd84abb872c32
Diego Argueta <620513-dargueta@users.noreply.github.com>
2020-05-20 23:38:05 -0700

    Add EFI shim

---
+++ include/mcinternal/efi.h
@@ -0,0 +1,44 @@
+/**
+ * Dummy declarations of datatypes for systems without EFI headers.
+ *
+ * Only library C files should include this; it must never be exposed to client
+ * code using the library. For systems with EFI headers this includes `efi.h`
+ * and `efilib.h`.
+ *
+ * When compiling this library without EFI support, this header defines a few
+ * datatypes defined in the EFI headers so we don't end up with hella "undeclared
+ * identifier" errors in code that uses @ref MetalCRuntimeInfo.
+ *
+ * @file efi.h
+ */
+
+#ifndef INCLUDE_MCINTERNAL_EFI_H_
+#define INCLUDE_MCINTERNAL_EFI_H_
+
+
+#include <metalc/metalc.h>
+
+
+#if METALC_HAVE_EFI_H
+    #include <efi.h>
+#endif
+
+#if METALC_HAVE_EFILIB_H
+    #include <efilib.h>
+#endif
+
+
+/* The EFI headers don't exist so we must be compiling in bare metal mode. */
+#if !METALC_HAVE_EFI_H
+    typedef void *EFI_HANDLE;
+    typedef struct {
+        /**
+         * A useless pointer; ISO C requires structs to have at least one member.
+         */
+        void *dummy;
+    } EFI_SYSTEM_TABLE;
+    typedef unsigned long EFI_STATUS;
+#endif
+
+
+#endif  /* INCLUDE_MCINTERNAL_EFI_H_ */
