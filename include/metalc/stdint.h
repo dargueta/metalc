22946f85a3c93900d89acd580869523c1fc16ff6
Diego Argueta <620513-dargueta@users.noreply.github.com>
2021-01-10 14:42:56 -0800

    Overhaul stdint.h to make it more robust

--- include/metalc/stdint.h
+++ include/metalc/stdint.h
@@ -6,106 +6,176 @@
 
 #if METALC_HAVE_STDINT_H
     /* Always defer to the platform's header file if present. GCC-compatible
-     * compilers will provide this for us. */
+     * compilers will provide this for us. In the past I've had trouble getting
+     * it to work with Visual Studio so I have a backup plan below. */
     #include <stdint.h>
-#elif defined _MSC_VER
-    /* No stdint.h but Microsoft-compatible compilers provide some non-standard
-     * predefined types for us. */
-    typedef __int8 int8_t;
-    typedef __int16 int16_t;
-    typedef __int32 int32_t;
-    typedef __int64 int64_t;
-    typedef unsigned __int8 uint8_t;
-    typedef unsigned __int16 uint16_t;
-    typedef unsigned __int32 uint32_t;
-    typedef unsigned __int64 uint64_t;
-    typedef int int_least8_t;
-    typedef int int_least16_t;
-    typedef int int_least32_t;
-    typedef int64_t int_least64_t;
-    typedef unsigned int uint_least8_t;
-    typedef unsigned int uint_least16_t;
-    typedef unsigned int uint_least32_t;
-    typedef uint64_t uint_least64_t;
 #elif METALC_HAVE_LIMITS_H
-    /* We don't have stdint.h and no predefined sized types, so we gotta figure
-     * it out ourselves. */
+    /* We don't have stdint.h so we gotta figure it out ourselves. */
     #include <limits.h>
 
-    typedef signed char int8_t;
-    typedef unsigned char uint8_t;
-    typedef int8_t int_least8_t;
-    typedef uint8_t uint_least8_t;
+    typedef signed char int_least8_t;
+    typedef unsigned char uint_least8_t;
+
+    #define INT_LEAST8_MIN      SCHAR_MIN
+    #define INT_LEAST8_MAX      SCHAR_MAX
+    #define UINT_LEAST8_MAX     UCHAR_MAX
+
+    typedef signed short int_least16_t;
+    typedef unsigned short uint_least16_t;
+
+    #define INT_LEAST16_MIN     SHRT_MIN
+    #define INT_LEAST16_MAX     SHRT_MAX
+    #define UINT_LEAST16_MAX    USHRT_MAX
+
+    #if SHRT_MAX >= 0x7FFFFFFF
+        /* A short is at least 32 bits. This is rare but it does happen in some
+         * specialized systems. */
+        typedef signed short int_least32_t;
+        typedef unsigned short uint_least32_t;
+
+        #define INT_LEAST32_MIN     SHRT_MIN
+        #define INT_LEAST32_MAX     SHRT_MAX
+        #define UINT_LEAST32_MAX    USHRT_MAX
+    #elif INT_MAX >= 0x7FFFFFFF
+        /* Int is at least 32 bits */
+        typedef signed int int_least32_t;
+        typedef unsigned int uint_least32_t;
+
+        #define INT_LEAST32_MIN     INT_MIN
+        #define INT_LEAST32_MAX     INT_MAX
+        #define UINT_LEAST32_MAX    UINT_MAX
+    #else
+        /* Int is the same size as a short (probably 16 bits), which means we
+         * must use a long for 32 bits. */
+        typedef signed long int_least32_t;
+        typedef unsigned long uint_least32_t;
+
+        #define INT_LEAST32_MIN     LONG_MIN
+        #define INT_LEAST32_MAX     LONG_MAX
+        #define UINT_LEAST32_MAX    ULONG_MAX
+    #endif
+
+    #if INT_MAX > 0x7FFFFFFF
+        /* An int is greater than 32 bits. Rare, but again, it does happen. It
+         * is VERY unlikely nowadays that this will be anything but 64 bits, so
+         * we're going to take a gamble. */
+        typedef signed int int_least64_t;
+        typedef unsigned int uint_least64_t;
+
+        #define INT_LEAST64_MIN     INT_MIN
+        #define INT_LEAST64_MAX     INT_MAX
+        #define UINT_LEAST64_MAX    UINT_MAX
+    #elif LONG_MAX > 0x7FFFFFFF
+        /* `long` is greater than 32 bits, assume it's 64. */
+        typedef signed long int_least64_t;
+        typedef unsigned long uint_least64_t;
+
+        #define INT_LEAST64_MIN     LONG_MIN
+        #define INT_LEAST64_MAX     LONG_MAX
+        #define UINT_LEAST64_MAX    ULONG_MAX
+    #elif defined(LLONG_MAX)
+        /* Compiler defines a `long long` which by definition must be at least
+         * 64 bits. */
+        typedef signed long long int_least64_t;
+        typedef unsigned long long uint_least64_t;
+
+        #define INT_LEAST64_MIN     LLONG_MIN
+        #define INT_LEAST64_MAX     LLONG_MAX
+        #define UINT_LEAST64_MAX    ULLONG_MAX
+    /* else: No way to define a 64-bit integer. We're probably compiling for a
+     * 16-bit system. */
+    #endif
+
+    #if METALC_ARCH_BITS == 16
+        typedef int_least16_t intptr_t;
+        typedef uint_least16_t uintptr_t;
+        typedef int_least16_t ptrdiff_t;
+
+        #define INTPTR_MIN  INT_LEAST16_MIN
+        #define INTPTR_MAX  INT_LEAST16_MAX
+        #define UINTPTR_MAX UINT_LEAST16_MAX
+    #elif METALC_ARCH_BITS == 32
+        typedef int_least32_t intptr_t;
+        typedef uint_least32_t uintptr_t;
+        typedef int_least32_t ptrdiff_t;
+
+        #define INTPTR_MIN  INT_LEAST32_MIN
+        #define INTPTR_MAX  INT_LEAST32_MAX
+        #define UINTPTR_MAX UINT_LEAST32_MAX
+    #else   /* METALC_ARCH_BITS == 64 */
+        typedef int_least64_t intptr_t;
+        typedef uint_least64_t uintptr_t;
+        typedef int_least64_t ptrdiff_t;
+
+        #define INTPTR_MIN  INT_LEAST64_MIN
+        #define INTPTR_MAX  INT_LEAST64_MAX
+        #define UINTPTR_MAX UINT_LEAST64_MAX
+    #endif
+
+    /* This won't be accurate for systems that support integers greater than 64
+     * bits, but it's good enough... right? */
+    #if defined(INT_LEAST64_MAX)
+        typedef int_least64_t intmax_t;
+        typedef uint_least64_t uintmax_t;
+
+        #define INTMAX_MIN  INT_LEAST64_MIN
+        #define INTMAX_MAX  INT_LEAST64_MAX
+        #define UINTMAX_MAX UINT_LEAST64_MAX
+    #elif defined(INT_LEAST32_MAX)
+        typedef int_least32_t intmax_t;
+        typedef uint_least32_t uintmax_t;
+
+        #define INTMAX_MIN  INT_LEAST32_MIN
+        #define INTMAX_MAX  INT_LEAST32_MAX
+        #define UINTMAX_MAX UINT_LEAST32_MAX
+    #else
+        /* Super unlikely but theoretically possible, e.g. if you're compiling
+         * for a z80 or something (which we don't have support for yet). */
+        typedef int_least16_t intmax_t;
+        typedef uint_least16_t uintmax_t;
+
+        #define INTMAX_MIN  INT_LEAST16_MIN
+        #define INTMAX_MAX  INT_LEAST16_MAX
+        #define UINTMAX_MAX UINT_LEAST16_MAX
+    #endif
+
+    #if UCHAR_MAX == 255
+        typedef unsigned char uint8_t;
+        typedef signed char int8_t;
+        #define UINT8_MAX   255
+        #define INT8_MIN    -127
+        #define INT8_MAX    127
+    #endif
 
     #if USHRT_MAX == 65535
-        typedef short int16_t;
         typedef unsigned short uint16_t;
-        typedef int16_t int_least16_t;
-        typedef uint16_t uint_least16_t;
-    #else
-        #error "A `short` isn't 16 bits, don't know how to typedef a 16-bit integer."
+        typedef signed short int16_t;
+        #define UINT16_MAX  65535
+        #define INT16_MIN   -32767
+        #define INT16_MAX   32767
     #endif
 
-    #if UINT_MAX < 4294967295UL
-        /* `int` is 16 bits, so `long` must be 32...right? */
-        #define INT_IS_16_BITS
-        #if ULONG_MAX == 4294967295UL
-            /* int is 16 bits, long is 32 bits */
-            typedef long int32_t;
-            typedef unsigned long uint32_t;
-            typedef int32_t int_least32_t;
-            typedef uint32_t uint_least32_t;
-        #else
-            #error "`int` is 16 bits, `long` isn't 32 bits, not sure how to define a 32-bit integer."
-        #endif
-    #elif UINT_MAX == 4294967295UL
-        typedef int int32_t;
-        typedef unsigned int uint32_t;
-        typedef int32_t int_least32_t;
-        typedef uint32_t uint_least32_t;
-    #elif UINT_MAX > 4294967295UL
-        #error "An `int` is more than 32 bits, don't know how to typedef a 32-bit integer."
+    #if defined(UINT_LEAST32_MAX) && UINT_LEAST32_MAX == 0xFFFFFFFF
+        typedef uint_least32_t uint32_t;
+        typedef int_least32_t int32_t;
+        #define UINT32_MAX  0xFFFFFFFF
+        #define INT32_MIN   -0x7FFFFFFF
+        #define INT32_MAX   0x7FFFFFFF
     #endif
 
-    #if ULONG_MAX > UINT_MAX
-        /* A long is bigger than an int. */
-        #ifdef INT_IS_16_BITS
-            /* Long is 32 bits, need a `long long` for 64-bit integers. */
-            #ifdef ULLONG_MAX
-                /* `long long` supported; by definition it's at least 64 bits. */
-                typedef long long int64_t;
-                typedef unsigned long long uint64_t;
-                typedef int64_t int_least64_t;
-                typedef uint64_t uint_least64_t;
-            #else
-                #error "Can't define a 64-bit integer: `long` is 32 bits and compiler doesn't support `long long`"
-            #endif
-        #else
-            /* `long` is bigger than an `int` and an `int` is 32 bits, thus a
-             * `long` is (probably) 64 bits. */
-            typedef long int64_t;
-            typedef unsigned long uint64_t;
-            typedef int64_t int_least64_t;
-            typedef uint64_t uint_least64_t;
-        #endif
-    #else
-        /* A long is the same size as an int. If the compiler defines `long long`
-         * then that must be our 64-bit integer. */
-        #if defined ULLONG_MAX
-            typedef long long int64_t;
-            typedef unsigned long long uint64_t;
-            typedef int64_t int_least64_t;
-            typedef uint64_t uint_least64_t;
-        #else
-            #error "Can't define a 64-bit integer: `long` is the same size as `int` and compiler doesn't support `long long`"
-        #endif
+    #if defined(UINT_LEAST64_MAX) && UINT_LEAST64_MAX == 0xFFFFFFFFFFFFFFFF
+        typedef uint_least64_t uint64_t;
+        typedef int_least64_t int64_t;
+        #define UINT64_MAX  0xFFFFFFFFFFFFFFFF
+        #define INT64_MIN   -0x7FFFFFFFFFFFFFFF
+        #define INT64_MAX   0x7FFFFFFFFFFFFFFF
     #endif
+
+    /* We're not going to define int_fast*_t because we don't know enough about
+     * the hardware. */
 #else
-    #error "Can't determine integer sizes; compiler doesn't provide limits.h."
-#endif
-
-#ifdef INT_IS_16_BITS
-    #undef INT_IS_16_BITS
+    /* This should never happen */
+    #error Can't determine integer sizes; compiler doesn't provide limits.h.
 #endif
 
 #endif  /* INCLUDE_METALC_STDINT_H_ */
