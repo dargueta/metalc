03a3601e7f7b47ba256e9c1ca74cc30e90087693
Diego Argueta <dargueta@users.noreply.github.com>
2023-04-21 19:09:30 -0700

    Add buffering mode macros to stdio.h

--- include/metalc/bits/stdio.h
+++ include/metalc/bits/stdio.h
@@ -4,13 +4,16 @@
 #include "../stddef.h"
 
 #define mclib_BUFSIZ          8192    /* glibc uses this value so we might as well */
-#define mclib_EOF             -1
+#define mclib_EOF             (-1)
 #define mclib_FILENAME_MAX    256     /* Literally arbitrary */
 #define mclib_FOPEN_MAX       16      /* Also arbitrary but glibc has this value */
 #define mclib_L_tmpnam        32      /* Continue the arbitraryness */
 #define mclib_SEEK_SET    0
 #define mclib_SEEK_CUR    1
 #define mclib_SEEK_END    2
+#define mclib_IONBF 0
+#define mclib_IOLBF 1
+#define mclib_IOFBF 2
 
 typedef ssize_t mclib_off_t;
 typedef ssize_t mclib_fpos_t;
@@ -26,6 +29,9 @@ typedef ssize_t mclib_fpos_t;
     #define SEEK_SET        mclib_SEEK_SET
     #define SEEK_CUR        mclib_SEEK_CUR
     #define SEEK_END        mclib_SEEK_END
+    #define _IONBF          mclib_IONBF
+    #define _IOLBF          mclib_IOLBF
+    #define _IOFBF          mclib_IOFBF
 
     typedef mclib_off_t off_t;
     typedef mclib_fpos_t fpos_t;
