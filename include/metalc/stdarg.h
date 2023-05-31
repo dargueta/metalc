08546b4657e0806eaff5f6fe4ba19db02aad9de0
Diego Argueta <dargueta@users.noreply.github.com>
2023-05-30 22:05:29 -0700

    Rip out checks for required freestanding headers

--- include/metalc/stdarg.h
+++ include/metalc/stdarg.h
@@ -1,13 +1,7 @@
 #ifndef INCLUDE_METALC_STDARG_H_
 #define INCLUDE_METALC_STDARG_H_
 
-#include "metalc.h"
-
-#if METALC_HAVE_STDARG_H
-    #include <stdarg.h>
-#else
-    /* We may eventually implement this ourselves but it's highly unlikely */
-    #error "Compiler must provide stdarg.h but it doesn't."
-#endif  /* METALC_HAVE_STDARG_H */
+// The compiler is required to provide this in freestanding mode
+#include <stdarg.h>
 
 #endif  /* INCLUDE_METALC_STDARG_H_ */
